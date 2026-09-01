9430f6770211a331ebe2b294ddd3d80b76e60e41
Ricardo Zgit config --global user.name Ricardo <kleyric@gmail.com>
2026-09-01 00:33:39 -0500

    Creacion de constructores de la ClaseHija Jugador

--- src/proyecto_segundo_corte/Jugador.java
+++ src/proyecto_segundo_corte/Jugador.java
@@ -1,7 +1,30 @@
 package proyecto_segundo_corte;
 public class Jugador extends Personajes{
-    private String clase = "";
-    private int dinero = 0;
-    private double xp = 0;
+    private String clase;
+    private int dinero;
+    private double xp;
     private char rango;
+    
+    public Jugador(String nombre, int nivel, double vida, boolean vivo, String clase, int dinero, double xp, char rango){
+        super(nombre, nivel, vida, vivo);
+        this.clase = clase;
+        this.dinero = dinero;
+        this.xp = xp;
+        this.rango = rango;
+    }
+    
+    public Jugador(String nombre, int nivel, String clase, char rango){
+        super(nombre, nivel);
+        this.clase = clase;
+        this.rango = rango;
+        this.xp = 0;
+        this.dinero = 0;
+    }
+    
+    public Jugador(){
+        this.clase = "Sin clase";
+        this.rango = '/';
+        this.xp = 0;
+        this.dinero = 0;
+    }
 }
--- src/proyecto_segundo_corte/Personajes.java
+++ src/proyecto_segundo_corte/Personajes.java
@@ -1,8 +1,8 @@
 package proyecto_segundo_corte;
 public class Personajes {
-    private String nombre = "";
-    private int nivel = 0;
-    private double vida = 0;
+    private String nombre;
+    private int nivel;
+    private double vida;
     private boolean vivo;
     
     public Personajes(String nombre, int nivel, double vida, boolean vivo){
@@ -13,11 +13,11 @@ public class Personajes {
     }
     
     public Personajes(String nombre, int nivel){
-        this(nombre, nivel, 100, true);
+        this(nombre, nivel, 0, true);
     }
     
     public Personajes(){
-        this("Sin nombre", 1, 100, true);
+        this("Sin nombre", 0, 0, true);
     }
     
     public String getNombre(){
