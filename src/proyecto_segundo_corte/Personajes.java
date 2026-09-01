47dc6d2a4eac535a720302e96269adc129157eb0
Ricardo Zgit config --global user.name Ricardo <kleyric@gmail.com>
2026-09-01 00:07:21 -0500

    Creacion de constructores de SuperClase Personajes

--- src/proyecto_segundo_corte/Personajes.java
+++ src/proyecto_segundo_corte/Personajes.java
@@ -4,4 +4,19 @@ public class Personajes {
     private int nivel = 0;
     private double vida = 0;
     private boolean vivo;
+    
+    public Personajes(String nombre, int nivel, double vida, boolean vivo){
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.vida = vida;
+        this.vivo = vivo;
+    }
+    
+    public Personajes(String nombre, int nivel){
+        this(nombre, nivel, 100, true);
+    }
+    
+    public Personajes(){
+        this("Sin nombre", 1, 100, true);
+    }
 }
