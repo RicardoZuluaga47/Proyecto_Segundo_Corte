a51be2b21d3d9d357abc0fc021ad5e8272172d9c
Ricardo Zgit config --global user.name Ricardo <kleyric@gmail.com>
2026-09-01 00:42:11 -0500

    Creacion de constructores de la ClaseHija Enemigo

--- src/proyecto_segundo_corte/Enemigo.java
+++ src/proyecto_segundo_corte/Enemigo.java
@@ -1,7 +1,30 @@
 package proyecto_segundo_corte;
 public class Enemigo extends Personajes{
-    private String raza = "";
-    private int recompensa = 0;
-    private double daño = 0;
+    private String raza;
+    private int recompensa;
+    private double daño;
     private boolean jefe;
+    
+    public Enemigo(String nombre, int nivel, double vida, boolean vivo, String raza, int recompensa, double daño, boolean jefe){
+        super(nombre, nivel, vida, vivo);
+        this.raza = raza;
+        this.recompensa = recompensa;
+        this.daño = daño;
+        this.jefe = jefe;
+    }
+    
+    public Enemigo(String nombre, int nivel, String raza, boolean jefe){
+        super(nombre, nivel);
+        this.raza = raza;
+        this.recompensa = 0;
+        this.daño = 0;
+        this.jefe = jefe;
+    }
+    
+    public Enemigo(){
+        this.raza = "Sin raza";
+        this.recompensa = 0;
+        this.daño = 0;
+        this.jefe = false;
+    }
 }
