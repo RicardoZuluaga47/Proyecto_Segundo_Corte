3f90c886448856dd5bb8fe958f063c0f85c3c6f8
Ricardo Zgit config --global user.name Ricardo <kleyric@gmail.com>
2026-09-01 00:52:39 -0500

    Creacion de getters y setters de la ClaseHija NPC

--- src/proyecto_segundo_corte/NPC.java
+++ src/proyecto_segundo_corte/NPC.java
@@ -27,4 +27,32 @@ public class NPC extends Personajes{
         this.dificultad = 0;
         this.estado = '/';
     }
+    
+    public String getMision(){
+        return mision;
+    }
+    public void setMision(String mision){
+        this.mision = mision;
+    }
+    
+    public int getRecompensa(){
+        return recompensa;
+    }
+    public void setRecompensa(int recompensa){
+        this.recompensa = recompensa;
+    }
+    
+    public double getDificultad(){
+        return dificultad;
+    }
+    public void setDificultad(double dificultad){
+        this.dificultad = dificultad;
+    }
+    
+    public char getEstado(){
+        return estado;
+    }
+    public void setEstado(char estado){
+        this.estado = estado;
+    }
 }
