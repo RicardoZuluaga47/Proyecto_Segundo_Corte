7ace2947d1d741a8567c23c3dca4dfbdcdc987b3
Ricardo Zgit config --global user.name Ricardo <kleyric@gmail.com>
2026-09-01 00:49:31 -0500

    Creacion de constructores de la ClaseHija NPC

--- src/proyecto_segundo_corte/NPC.java
+++ src/proyecto_segundo_corte/NPC.java
@@ -1,7 +1,30 @@
 package proyecto_segundo_corte;
 public class NPC extends Personajes{
-    private String mision = "";
-    private int recompensa = 0;
-    private double dificultad = 0;
+    private String mision;
+    private int recompensa;
+    private double dificultad;
     private char estado;
+    
+    public NPC(String nombre, int nivel, double vida, boolean vivo, String mision, int recompensa, double dificultad, char estado){
+        super(nombre, nivel, vida, vivo);
+        this.mision = mision;
+        this.recompensa = recompensa;
+        this.dificultad = dificultad;
+        this.estado = estado;
+    }
+    
+    public NPC(String nombre, int nivel, String mision, double dificultad){
+        super(nombre, nivel);
+        this.mision = mision;
+        this.recompensa = 0;
+        this.dificultad = dificultad;
+        this.estado = '/';
+    }
+    
+    public NPC(){
+        this.mision = "Sin mision";
+        this.recompensa = 0;
+        this.dificultad = 0;
+        this.estado = '/';
+    }
 }
