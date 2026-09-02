f4d50841b57938dba16f7b2e23f87dbb6e792b40
Ricardo Zgit config --global user.name Ricardo <kleyric@gmail.com>
2026-09-01 23:43:25 -0500

    Correccion de estructura

--- src/proyecto_segundo_corte/Proyecto_Segundo_Corte.java
+++ src/proyecto_segundo_corte/Proyecto_Segundo_Corte.java
@@ -1,24 +1,28 @@
 package proyecto_segundo_corte;
 public class Proyecto_Segundo_Corte {
     public static void main(String[] args) {
+        /*Uso constructores vacios ya que son entidades(personajes, jugadores, etc)
+        que son nuevas y no se tiene informacion de ellas*/
         Personajes personaje1 = new Personajes();
-        Personajes personaje2 = new Personajes("Lucian", 16);
-        Personajes personaje3 = new Personajes("Selene", 32, 418, true);
-        
         Jugador player1 = new Jugador();
-        Jugador player2 = new Jugador("Sylas", 21, "Picaro", 'D');
-        Jugador player3 = new Jugador("Ragnar", 48, 653.0, true, "Barbaro", 4297, 509.72, 'C');
-        
         Enemigo nomuerto1 = new  Enemigo();
-        Enemigo nomuerto2 = new  Enemigo("Esqueleto", 17, "No-Muerto", false);
-        Enemigo jefe1 = new  Enemigo("Nekros", 50, 2470.0, false, "Nigromante", 1500, 68.85, true);
-        
         NPC npc1 = new NPC();
-        NPC granjero1 = new NPC("Valeria", 73, "Mata al mago de la peste", 'P');
-        NPC herrero1 = new NPC("Serah", 35, 100.0, true, "Acaba con Nekros el Nigromante", 1500, 6.4, 'C');
         
-        //Constructores vacios, valores predeterminados
-        System.out.println("ESTADO: PREDETERMINADO (SIN ASIGNACION DE VALORES)");
+        /*Uso constructores incompletos ya que son entidades(personajes, jugadores, etc)
+        que estan en proceso de conocimiento, osea aun no tienen toda la informacion de ellas*/
+        Personajes personaje2 = new Personajes("Lucian", 16);
+        Jugador player2 = new Jugador("Sylas", 21, "Picaro", 'D');
+        Enemigo nomuerto2 = new  Enemigo("Esqueleto", 17, "No-Muerto", false);
+        NPC granjero1 = new NPC("Valeria", 73, "Mata al mago de la peste", 'P');
+        
+        /*Uso constructores Completos ya que son entidades(personajes, jugadores, etc)
+        que son conocidas y se tiene toda la informacion de estas*/
+        Personajes personaje3 = new Personajes("Selene", 32, 418, true);
+        Jugador player3 = new Jugador("Ragnar", 48, 653.0, true, "Barbaro", 4297, 509.72, 'C');
+        Enemigo jefe1 = new  Enemigo("Nekros", 50, 2470.0, false, "Nigromante", 1500, 68.85, true);
+        NPC herrero1 = new NPC("Serah", 35, 100.0, true, "Acaba con Nekros el Nigromante", 1500, 6.4, 'C');
+
+        System.out.println("ESTADO: DESCONOCIDOS");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println(personaje1);
         System.out.println("---------------------------------------------------");
@@ -29,8 +33,7 @@ public class Proyecto_Segundo_Corte {
         System.out.println(npc1);
         System.out.println("***************************************************");
         
-        //Constructores incompletos, falta de conocimiento de todos los datos
-        System.out.println("ESTADO: INCOMPLETO (FALTA DE VALORES)");
+        System.out.println("ESTADO: BUSCANDO INFORMACION");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println(personaje2);
         System.out.println("---------------------------------------------------");
@@ -42,8 +45,7 @@ public class Proyecto_Segundo_Corte {
         System.out.println("");
         System.out.println("***************************************************");
         
-        //Constructores completos, todos los datos asignados correctamente
-        System.out.println("ESTADO: COMPLETO");
+        System.out.println("ESTADO: IDENTIFICADO");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println(personaje3);
         System.out.println("---------------------------------------------------");
